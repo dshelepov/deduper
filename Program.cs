156dc68a68909a6453234d56f03117ccfbc8cad1
Daniel Shelepov <Daniel Shelepov>
2017-02-20 18:00:58 -0800

    FW: initial state
    Set up C# project, minimal doc & licensing.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (C) Daniel Shelepov.  All rights reserved.
+using System;
+
+namespace Deduper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# deduper
+## Deduper
 Duplicate file finder
+
+##### Copyright
+
+Copyright © Daniel Shelepov.  All rights reserved.
+
+##### License
+Distributed under GNU-GPL.
+For commercial licensing, contact the author.
